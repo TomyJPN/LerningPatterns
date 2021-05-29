7042570b3aa47eb52b931f43eca193ae63ed991c
DESKTOP-GALLERIA\TOMY <masaya120331@gmail.com>
2021-05-29 15:46:40 +0900

    シンプルなstateパターン利用

---
+++ LearningPatterns/Assets/Scenes/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private static readonly StateStanding stateStanding = new StateStanding();
+    private static readonly StateJumping stateJumping = new StateJumping();
+
+    [SerializeField]
+    private Rigidbody rigidbody;
+
+    /// <summary>現在のState</summary>
+    private PlayerStateBase _currentState;
+
+    /// <summary>現在のState名</summary>
+    public string CurrentStateName => _currentState.ToString();
+
+    void Start()
+    {
+        _currentState = stateStanding;
+    }
+
+    void Update()
+    {
+        _currentState.OnUpdate(this);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // ここでは簡易的に何かに当たれば地面衝突とする
+        ChangeState(stateStanding);
+    }
+
+
+    /// <summary>
+    /// Stateの変更
+    /// </summary>
+    private void ChangeState(PlayerStateBase nextState)
+    {
+        _currentState.OnExit(this, nextState);
+        _currentState = nextState;
+        _currentState.OnEnter(this, _currentState);
+    }
+
+    /// <summary>
+    /// 立ち状態
+    /// </summary>
+    private class StateStanding : PlayerStateBase
+    {
+        public override void OnUpdate(Player owner)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                owner.ChangeState(stateJumping);
+            }
+        }
+    }
+
+    /// <summary>
+    /// ジャンプ状態
+    /// </summary>
+    private class StateJumping : PlayerStateBase
+    {
+        private const int jumpPower = 5;
+
+        public override void OnEnter(Player owner, PlayerStateBase prevState)
+        {
+            owner.rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            base.OnEnter(owner, prevState);
+        }
+    }
+}
---
+++ LearningPatterns/Assets/Scenes/Scripts/PlayerStateBase.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// State抽象クラス
+/// </summary>
+public abstract class PlayerStateBase
+{
+    /// <summary>
+    /// ステート開始時
+    /// </summary>
+    public virtual void OnEnter(Player owner, PlayerStateBase prevState)
+    {
+        UnityEngine.Debug.Log(owner.CurrentStateName + "を開始");
+    }
+
+    /// <summary>
+    /// 毎フレーム
+    /// </summary>
+    public virtual void OnUpdate(Player owner){ }
+
+    /// <summary>
+    /// ステート終了時
+    /// </summary>
+    public virtual void OnExit(Player owner, PlayerStateBase nextState)
+    {
+        UnityEngine.Debug.Log(owner.CurrentStateName + "を終了");
+    }
+}
